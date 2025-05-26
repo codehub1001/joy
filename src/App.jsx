a209d178be50e1f3e7cfecc96b76e2783ca4d114
chuka <oluwachukkie@gmail.com>
2025-05-26 17:04:38 +0100

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from './components/header'
+import Header from './components/Header'
 import Banner from './components/Banner'
 import Messages from './pages/Messages'
 
